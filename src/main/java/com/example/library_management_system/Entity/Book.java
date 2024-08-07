015fa5c75f652155bfb314b92e9e7784f1487b85
Assem Salama <assem.salama@icloud.com>
2024-08-07 20:45:07 +0300

    First commit Entities added

---
+++ src/main/java/com/example/library_management_system/Entity/Book.java
@@ -0,0 +1,48 @@
+package com.example.library_management_system.Entity;
+
+import java.time.Year;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String author;
+
+    private Year publicationYear;
+    
+    private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public Year getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(Year publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    } 
+    
+}
---
+++ src/main/java/com/example/library_management_system/Entity/BorrowingRecord.java
@@ -0,0 +1,62 @@
+package com.example.library_management_system.Entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class BorrowingRecord {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "patron_id")
+    private Patron patron;
+
+    private LocalDate borrowingDate;
+    
+    private LocalDate returnDate;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Book getBook() {
+        return book;
+    }
+    public void setBook(Book book) {
+        this.book = book;
+    }
+    public Patron getPatron() {
+        return patron;
+    }
+    public void setPatron(Patron patron) {
+        this.patron = patron;
+    }
+    public LocalDate getBorrowingDate() {
+        return borrowingDate;
+    }
+    public void setBorrowingDate(LocalDate borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+    
+}
