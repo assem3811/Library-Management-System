26d434472a630c2e0a1d3fae78253d7d25b0eeba
Assem Salama <assem.salama@icloud.com>
2024-08-09 01:29:35 +0300

    Last Commit

--- src/main/java/com/example/library_management_system/Controller/BorrowingController.java
+++ src/main/java/com/example/library_management_system/Controller/BorrowingController.java
@@ -3,6 +3,7 @@ package com.example.library_management_system.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class BorrowingController {
     @PostMapping("/borrow/{bookId}/patron/{patronId}")
     public BorrowingRecord borrowBook(@PathVariable Long bookId, @PathVariable Long patronId) {
         return borrowingService.borrowBook(bookId, patronId);
+    }
+
+    @PutMapping("/return/{bookId}/patron/{patronId}")
+    public BorrowingRecord returnBook(@PathVariable Long bookId, @PathVariable Long patronId) {
+        return borrowingService.returnBook(bookId, patronId);
     }
 }
